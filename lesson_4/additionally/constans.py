1aca65bd7e5be8cd90d888bd96fc21a7ce3121ee
ivg97 <gavr97.97@mail.ru>
2022-01-24 19:05:16 +0300

    тестирование сервера и клиента,  а также их функций

---
+++ lesson_4/additionally/constans.py
@@ -0,0 +1,23 @@
+'''
+CONSTANTS
+'''
+
+DEFAULT_HOST = '127.0.0.1'
+
+DEFAULT_PORT = 7777
+
+QUANTITY_CONNECTION = 3
+
+MAX_SIZE_SMS = 1024
+
+ENCODING = 'utf-8'
+
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
